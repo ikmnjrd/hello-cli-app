8842498537876c3c6ab1715c3eb8789e08043849
ike <40803799+ikmnjrd@users.noreply.github.com>
2022-11-12 02:04:15 +0900

    file read

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,19 @@ use clap::Parser;
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 struct Cli {
-  /// The pattern to look for
-  pattern: String,
-  /// The path to the file to read
-  path: std::path::PathBuf,
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
 }
 
-fn main() {
-    let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let result = std::fs::read_to_string("test.txt");
 
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+    let content = match result {
+        Ok(content) => { content },
+        Err(error) => { return Err(error.into()); }
+    };
+    println!("file content: {}", content);
+    Ok(())
 }
